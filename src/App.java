62614cc2c1c4fde104f6621e0cc59be2446e4240
geon <zqrc05@gmail.com>
2024-08-11 10:09:16 +0900

    깃테스트

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("확장등록되었음 깃 테스트!");
     }
 }
